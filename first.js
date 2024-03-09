289748db58ad08ec7e7ac5828449a5b7cb4359dc
PavansaiKaviti <146151676+PavansaiKaviti@users.noreply.github.com>
2024-03-09 10:29:10 -0600

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+// single function with loop
+//function fac(name) {
+//attachement(name);
+//return `\n${name} got a job`;}
+//function attachement(name) {
+// return console.log(`my name is ${name}.\n
+//i am a full stack developer.\n
+//${name} was searching for a job Now!\n`);}
+//module.exports = fac;
+// object export
---
+++ second.js
@@ -0,0 +1,2 @@
+const num1 = require("./first");
+console.log(num1("pavansai_kaviti"));
